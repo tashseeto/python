2bad92f87699a357dbc2188a9b540e4e333640b1
Natasha Seeto <tashseeto@gmail.com>
2020-07-21 16:47:34 +1000

    Question 4 complete

--- booleans-and-user-input-exercises.py
+++ booleans-and-user-input-exercises.py
@@ -1,41 +1,41 @@
-# print("Question 1")
+print("Question 1")
 
-# moths_in_house = True
+moths_in_house = True
 
-# if moths_in_house:
-#     print("Get the moths!")
+if moths_in_house:
+    print("Get the moths!")
 
-# moths_in_house = False
+moths_in_house = False
 
-# if not moths_in_house:
-#     print("No threats detected.")
+if not moths_in_house:
+    print("No threats detected.")
 
 
-# print("Question 2")
+print("Question 2")
 
-# moths_in_house = True
-# mitch_is_home = True
+moths_in_house = True
+mitch_is_home = True
 
-# if moths_in_house and mitch_is_home:
-#     print("Hooman! Help me get the moths!")
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
 
-# moths_in_house = False
-# mitch_is_home = False
+moths_in_house = False
+mitch_is_home = False
 
-# if not moths_in_house and not mitch_is_home:
-#     print("No threats detected.")
+if not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
 
-# moths_in_house = True
-# mitch_is_home = False
+moths_in_house = True
+mitch_is_home = False
 
-# if moths_in_house and not mitch_is_home:
-#     print("Meoooooooooooooooww! Hisssssss!")
+if moths_in_house and not mitch_is_home:
+    print("Meoooooooooooooooww! Hisssssss!")
 
-# moths_in_house = False
-# mitch_is_home = True
+moths_in_house = False
+mitch_is_home = True
 
-# if not moths_in_house and mitch_is_home:
-#     print("Climb on Mitch.")
+if not moths_in_house and mitch_is_home:
+    print("Climb on Mitch.")
 
 print("Question 3")
 
@@ -74,3 +74,14 @@ car_detected = True
 
 if light_colour and car_detected:
     print("Do nothing.")
+
+print("Question 4")
+
+height_required = 119
+height_prompt = input("How tall are you in cm?: ")
+height_prompt = int(height_prompt)
+
+if height_prompt > height_required:
+    print("Hop on!")
+else:
+    print("Sorry, not today!")
