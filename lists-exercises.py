20835a904d69b66a647c06ef69dd58b64dcad252
Natasha Seeto <tashseeto@gmail.com>
2020-07-21 19:34:36 +1000

    Question 1 complete

---
+++ lists-exercises.py
@@ -0,0 +1,27 @@
+# print("Question 1")
+
+# foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry",["carrot", "cauliflower", "pumpkin"], "passionfruit", "mango", "kiwifruit"]
+
+# print(foods)
+
+# print(foods[0])
+# print(foods[2])
+# print(foods[-1])
+# print(foods[0:3])
+# print(foods[7:])
+# # print(foods [-3:]) - alternate to above
+# print(foods[6][2])
+
+
+print("Question 2")
+
+mailing_list = [
+    ["Roary", "roary@moth.catchers"],
+    ["Remus", "remus@kapers.dog"],
+    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+    ["Biscuit", "biscuit@whippies.park"],
+    ["Rory", "rory@hippies.park"],
+]
+
+print(mailing_list)
+print(mailing_list[0][0]
