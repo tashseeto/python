1433051d62e7ee71bfc79299d797207e5869f2d4
Natasha Seeto <tashseeto@gmail.com>
2020-07-21 20:32:12 +1000

    Question 2 complete

--- lists-exercises.py
+++ lists-exercises.py
@@ -1,27 +1,31 @@
-# print("Question 1")
+# # print("Question 1")
 
-# foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry",["carrot", "cauliflower", "pumpkin"], "passionfruit", "mango", "kiwifruit"]
+# # foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry",["carrot", "cauliflower", "pumpkin"], "passionfruit", "mango", "kiwifruit"]
 
-# print(foods)
+# # print(foods)
 
-# print(foods[0])
-# print(foods[2])
-# print(foods[-1])
-# print(foods[0:3])
-# print(foods[7:])
-# # print(foods [-3:]) - alternate to above
-# print(foods[6][2])
+# # print(foods[0])
+# # print(foods[2])
+# # print(foods[-1])
+# # print(foods[0:3])
+# # print(foods[7:])
+# # # print(foods [-3:]) - alternate to above
+# # print(foods[6][2])
 
 
-print("Question 2")
+# print("Question 2")
 
-mailing_list = [
-    ["Roary", "roary@moth.catchers"],
-    ["Remus", "remus@kapers.dog"],
-    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
-    ["Biscuit", "biscuit@whippies.park"],
-    ["Rory", "rory@hippies.park"],
-]
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@hippies.park"],
+# ]
 
-print(mailing_list)
-print(mailing_list[0][0]
+# print(mailing_list)
+# print(f"{mailing_list[0][0]} : {mailing_list[0][1]}")
+# print(f"{mailing_list[1][0]} : {mailing_list[1][1]}")
+# print(f"{mailing_list[2][0]} : {mailing_list[2][1]}")
+# print(f"{mailing_list[3][0]} : {mailing_list[3][1]}")
+# print(f"{mailing_list[4][0]} : {mailing_list[4][1]}")
