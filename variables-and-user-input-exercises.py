3be914e7df9a538403b303e8a8f0264ce7a8fdce
Natasha Seeto <tashseeto@gmail.com>
2020-07-21 15:17:40 +1000

    Question 1 & Question 2

--- variables-and-user-input-exercises.py
+++ variables-and-user-input-exercises.py
@@ -1,11 +1,57 @@
-# Question 1
+print("Question 1")
 
-print("Integers")
+num1A = input("Please type 3 ") 
+num1B = input("Please type 9 ")
+num1A = int(num1A)
+num1B = int(num1B)
+product = num1A + num1B 
 
-first_int = 3
-second_int = 9
+print(f"Let's do some addition! {num1A} + {num1B} = {product}")
 
-print(first_int + second_int)
 
-# Question 2
+num1C = input ("Please type -3 ")
+num1B = input("Please type 9 ")
+num1C = int(num1C)
+num1B = int(num1B)
+product = num1C + num1B
+
+print(f"Let's do some more addition! {num1C} + {num1B} = {product}")
+
+num1D = input("Please type 3.0 ")
+num1E = input("Please type -9 ") 
+num1D = float(num1D)
+num1E = int(num1E)
+product = num1D + num1E
+
+print(f"Let's do even MORE addition! {num1D} + {num1E} = {product}")
+
+
+print("Question 2")
+
+num2A = input("Please type 3 ")
+num2B = input("Please type 9 ")
+num2A = int(num2A)
+num2B = int(num2B)
+product = num2A * num2B
+
+print(f"Let's do some multiplication! {num2A} * {num2B} = {product}")
+
+num2C = input("Please type -3 ")
+num2D = input("Please type 9 ")
+num2C = int(num2C)
+num2D = int(num2D)
+product = num2C * num2D
+
+print(f"Let's do some multiplication! {num2C} * {num2D} = {product}")
+
+num2E = input("Please type 3.0 ")
+num2F = input("Please type -9 ")
+num2E = float(num2E)
+num2F = int(num2F)
+product = num2E * num2F
+
+print(f"Let's do some multiplication! {num2E} * {num2F} = {product}")
+
+
+print("Question 3")
 
