971c5a17a803435bd965108a981fa7a18f477818
Natasha Seeto <tashseeto@gmail.com>
2020-07-21 16:03:39 +1000

    Question 1 & Question 2 complete

--- booleans-and-user-input-exercises.py
+++ booleans-and-user-input-exercises.py
@@ -1,1 +1,38 @@
 print("Question 1")
+
+moths_in_house = True
+
+if moths_in_house:
+    print("Get the moths!")
+
+moths_in_house = False
+
+if not moths_in_house:
+    print("No threats detected.")
+
+
+print("Question 2")
+
+moths_in_house = True
+mitch_is_home = True
+
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths!")
+
+moths_in_house = False
+mitch_is_home = False
+
+if not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
+
+moths_in_house = True
+mitch_is_home = False
+
+if moths_in_house and not mitch_is_home:
+    print("Meoooooooooooooooww! Hisssssss!")
+
+moths_in_house = False
+mitch_is_home = True
+
+if not moths_in_house and mitch_is_home:
+    print("Climb on Mitch.")
