654563b6af117b583f7c821207c2b3ea625dbd67
Natasha Seeto <tashseeto@gmail.com>
2020-07-21 15:31:43 +1000

    Question 3 completed

--- variables-and-user-input-exercises.py
+++ variables-and-user-input-exercises.py
@@ -1,57 +1,78 @@
-print("Question 1")
+# print("Question 1")
 
-num1A = input("Please type 3 ") 
-num1B = input("Please type 9 ")
-num1A = int(num1A)
-num1B = int(num1B)
-product = num1A + num1B 
+# num1A = input("Please type 3 ") 
+# num1B = input("Please type 9 ")
+# num1A = int(num1A)
+# num1B = int(num1B)
+# product = num1A + num1B 
 
-print(f"Let's do some addition! {num1A} + {num1B} = {product}")
-
-
-num1C = input ("Please type -3 ")
-num1B = input("Please type 9 ")
-num1C = int(num1C)
-num1B = int(num1B)
-product = num1C + num1B
-
-print(f"Let's do some more addition! {num1C} + {num1B} = {product}")
-
-num1D = input("Please type 3.0 ")
-num1E = input("Please type -9 ") 
-num1D = float(num1D)
-num1E = int(num1E)
-product = num1D + num1E
-
-print(f"Let's do even MORE addition! {num1D} + {num1E} = {product}")
+# print(f"{num1A} + {num1B} = {product}")
 
 
-print("Question 2")
+# num1C = input ("Please type -3 ")
+# num1B = input("Please type 9 ")
+# num1C = int(num1C)
+# num1B = int(num1B)
+# product = num1C + num1B
 
-num2A = input("Please type 3 ")
-num2B = input("Please type 9 ")
-num2A = int(num2A)
-num2B = int(num2B)
-product = num2A * num2B
+# print(f"num1C} + {num1B} = {product}")
 
-print(f"Let's do some multiplication! {num2A} * {num2B} = {product}")
+# num1D = input("Please type 3.0 ")
+# num1E = input("Please type -9 ") 
+# num1D = float(num1D)
+# num1E = int(num1E)
+# product = num1D + num1E
 
-num2C = input("Please type -3 ")
-num2D = input("Please type 9 ")
-num2C = int(num2C)
-num2D = int(num2D)
-product = num2C * num2D
-
-print(f"Let's do some multiplication! {num2C} * {num2D} = {product}")
-
-num2E = input("Please type 3.0 ")
-num2F = input("Please type -9 ")
-num2E = float(num2E)
-num2F = int(num2F)
-product = num2E * num2F
-
-print(f"Let's do some multiplication! {num2E} * {num2F} = {product}")
+# print(f"{num1D} + {num1E} = {product}")
 
 
-print("Question 3")
+# print("Question 2")
+
+# num2A = input("Please type 3 ")
+# num2B = input("Please type 9 ")
+# num2A = int(num2A)
+# num2B = int(num2B)
+# product = num2A * num2B
+
+# print(f"{num2A} * {num2B} = {product}")
+
+# num2C = input("Please type -3 ")
+# num2D = input("Please type 9 ")
+# num2C = int(num2C)
+# num2D = int(num2D)
+# product = num2C * num2D
+
+# print(f"{num2C} * {num2D} = {product}")
+
+# num2E = input("Please type 3.0 ")
+# num2F = input("Please type -9 ")
+# num2E = float(num2E)
+# num2F = int(num2F)
+# product = num2E * num2F
+
+# print(f"{num2E} * {num2F} = {product}")
+
+
+# print("Question 3")
+
+# num3A = input("Please type 10 ")
+# num3A = int(num3A)
+# productA = num3A * 1000
+# productA = num3A * 100000
+
+# print(f"{productA}m")
+# print(f"{productA}cm")
+
+# num3B = input("Please type 5.4 ")
+# num3B = float(num3B)
+# productB = (num3B) * 1000
+# productC = num3B * 100000
+# productB = int(productB)
+# productC = int(productC)
+
+# print(f"{num3B}km = {productB}m")
+# print(f"{num3B}km = {productC}cm")
+
+print("Question 4")
+
 
