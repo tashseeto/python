b9a8c1b680f8dbb97f6cd680763fc9f1b9a16439
Natasha Seeto <tashseeto@gmail.com>
2020-07-24 18:39:29 +1000

    attempting different logic to solve exercises

---
+++ loops-exercises.py
@@ -0,0 +1,24 @@
+print("Question 1")
+
+# number= input(int("Please enter a number: "))
+# product= number
+# for user_input in number:
+#     print(number)
+
+count = 0
+while count > 0:
+    numbers = input("Please enter a number: ")
+    numbers = int(numbers)
+
+    total = total + digits
+    
+    print(total)
+
+
+
+
+# while digits in digit_collection:
+#     print(digits)
+#     total = total + digits
+# print(total)
+    
