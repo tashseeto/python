a8ab35bc7d2eaf4bade410f43966edb30acf0b6c
Natasha Seeto <tashseeto@gmail.com>
2020-07-25 19:00:17 +1000

    question 3 complete

--- loops-exercises.py
+++ loops-exercises.py
@@ -14,13 +14,26 @@
 
 # print("Question 2")
 
-mailing_list = [
-    ["Roary", "roary@moth.catchers"],
-    ["Remus", "remus@kapers.dog"],
-    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
-    ["Biscuit", "biscuit@whippies.park"],
-    ["Rory", "rory@whippies.park"],
-]
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@whippies.park"],
+# ]
 
-for name in mailing_list:
-    print(f"{name[0]} : {name[1]}")
+# for name in mailing_list:
+#     print(f"{name[0]} : {name[1]}")
+
+
+#  print("Question 3")
+
+my_list = []
+
+counter = 0
+while counter < 3:
+    counter += 1
+    user_prompt = input("Please enter a name: ")
+    my_list.append(user_prompt)  
+print(my_list)
+
