613962347155ea450a9431ae5ea07525335d5dde
Natasha Seeto <tashseeto@gmail.com>
2020-07-25 22:57:14 +1000

    question 4 complete

--- loops-exercises.py
+++ loops-exercises.py
@@ -28,12 +28,41 @@
 
 #  print("Question 3")
 
-my_list = []
+# my_list = []
 
-counter = 0
-while counter < 3:
-    counter += 1
-    user_prompt = input("Please enter a name: ")
-    my_list.append(user_prompt)  
-print(my_list)
+# counter = 0
+# while counter < 3:
+#     counter += 1
+#     user_prompt = input("Please enter a name: ")
+#     my_list.append(user_prompt)  
+# for user_prompt in my_list:
+#     print(user_prompt)
+
+
+
+print("Question 4")
+
+groceries = [
+    ["Baby Spinach", 2.78],
+    ["Hot Chocolate", 3.70],
+    ["Crackers", 2.10],
+    ["Bacon", 9.00],
+    ["Carrots", 0.56],
+    ["Oranges", 3.08]
+]
+
+total = 0
+
+for item in groceries:
+    item_quantity = input(f"How many units would you like of this item {item[0]}? ")
+    item[1] = item[1] * int(item_quantity)
+    total += item[1]
+   
+total = f"${total:.2f}"
+
+print("====Izzy's Food Emporium====")
+for item in groceries:
+    print(f"{item[0]:<16} ${item[1]:7.2f}")
+print("============================")
+print(f"{total:>25}")
 
