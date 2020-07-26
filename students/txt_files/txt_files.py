fe22351b2980d4ba88a4fa5d11284492b9c619d1
Natasha Seeto <tashseeto@gmail.com>
2020-07-26 19:02:17 +1000

    question 1 really complete this time!

--- students/txt_files/txt_files.py
+++ students/txt_files/txt_files.py
@@ -1,11 +1,13 @@
+# Question 1
+
 names = []
 
 with open("names.txt") as txt_file:
-    for line in txt_file:
-        line = line.strip()
-        names.append(line)
+    for name in txt_file:
+        name = name.strip()
+        names.append(name)
 
-    for counter, name in enumerate(names, 1):
-            print(f"{counter}.  {name}")
-     
+for counter, name in enumerate(names, 1):
+    print(f"{counter}.  {name}")
+
 
