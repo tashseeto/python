515296f5004106c26fa4e89223e0f53d00ff306f
Natasha Seeto <tashseeto@gmail.com>
2020-07-27 14:41:57 +1000

    question 2b - complete - same resolution

--- students/exercise_data/csv_files.py
+++ students/exercise_data/csv_files.py
@@ -1,13 +1,24 @@
+# import csv
+
+# colours_20 = []
+
+# with open("colours_20.csv") as csv_file:
+#     reader = csv.reader(csv_file)
+#     for line in reader:
+#         colours_20.append(line)
+
+# for codes in colours_20:
+#     print(f"{codes[1].strip():16} {codes[2].strip():16} {codes[4].strip():16}")
+
+
 import csv
 
-colours_20 = []
+colours_213 = []
 
-with open("colours_20.csv") as csv_file:
+with open("colours_213.csv") as csv_file:
     reader = csv.reader(csv_file)
     for line in reader:
-        colours_20.append(line)
+        colours_213.append(line)
 
-
-
-for codes in colours_20:
+for codes in colours_213:
     print(f"{codes[1].strip():16} {codes[2].strip():16} {codes[4].strip():16}")
