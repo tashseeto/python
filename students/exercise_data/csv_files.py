a3d4372ee236ebce42611bd62b783d607900e749
Natasha Seeto <tashseeto@gmail.com>
2020-07-27 14:40:06 +1000

    question 2 complete - list splicing

--- students/exercise_data/csv_files.py
+++ students/exercise_data/csv_files.py
@@ -1,3 +1,13 @@
 import csv
 
 colours_20 = []
+
+with open("colours_20.csv") as csv_file:
+    reader = csv.reader(csv_file)
+    for line in reader:
+        colours_20.append(line)
+
+
+
+for codes in colours_20:
+    print(f"{codes[1].strip():16} {codes[2].strip():16} {codes[4].strip():16}")
