7cdab31615e1b29cfa7394ec8d5d2d22f65efc81
Natasha Seeto <tashseeto@gmail.com>
2020-07-28 17:04:09 +1000

    question 1 complete

--- functions-exercises.py
+++ functions-exercises.py
@@ -1,2 +1,10 @@
-def temperature(farenheit):
-    return f"temperature is {farenheit} degrees"
+# Question 1
+
+def temperature(celsius):
+    celsius = (celsius - 32) / 1.8
+    celsius = round(celsius)
+    print(celsius)
+
+temperature(32)
+temperature(113)
+temperature(26.6)
