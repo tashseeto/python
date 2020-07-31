fde512b208627ffb34e1cebcb9994f8fcf832b5f
Natasha Seeto <tashseeto@gmail.com>
2020-08-01 01:52:04 +1000

    Update functions-exercises.py

--- functions-exercises.py
+++ functions-exercises.py
@@ -44,8 +44,6 @@
 
 # Question 4
 
-
-
 groceries = [
     ["Baby Spinach", 2.78],
     ["Hot Chocolate", 3.70],
@@ -55,17 +53,97 @@ groceries = [
     ["Oranges", 3.08]
 ]
 
-list1 = groceries
 
+groceries = [
+    ["Baby Spinach", 2.78],
+    ["Hot Chocolate", 3.70],
+    ["Crackers", 2.10],
+    ["Bacon", 9.00],
+    ["Carrots", 0.56],
+    ["Oranges", 3.08],
+]
 def calculate_cost(unit_price, number_purchase):
-    total_unit_cost = unit_price * number_purchase
-    return total_unit_cost
+    costs = []
+    total_cost = 0
+    for items in groceries:
+        items_cost = float(unit_price) * int(number_purchase)
+        costs.append(items_cost)
+        total_cost += float(items_cost)
+    print(costs)
+    return f"${total_cost:.2f}"
 
-for item in list1:
-    quantity = int(input(f"How many units would you like of this item {item[0]}? "))
-    item.append(quantity)
-    total_unit_cost = float(calculate_cost(item[1],quantity))
-    item.append(total_unit_cost)
+for items in groceries:
+    quantity = input(f"How many {items[0]} did you buy? ")
+    price = items[1]
+
+calculation = calculate_cost(price, quantity)
+print(calculation)
+
+print()
+
+print("====Izzy's Food Emporium====")
+for items in groceries:
+    print(f"{items[0]:<17} ${items[1]:.2f}")
+print("============================")
+print(f"{calculation:>24}")
+print()
+
+
+
+
+
+
+
+# print(groceries)
+
+# def calculate_cost(unit_price, number_purchase):
+#     total_unit_cost = unit_price * number_purchase
+#     return total_unit_cost
+
+
+# total_unit_cost = 0
+
+# for item in groceries:
+#     item_quantity = input(f"How many units would you like of this item {item[0]}? ")
+#     item.append(item_quantity)
+#     item[1] = item[1] * int(item_quantity)
+#     total_unit_cost += item[1]
+
+# print(groceries)
+
+
+
+
+    
+#     item[1] = item[1] * int(item_quantity)
+#     total += item[1]
+   
+# total = f"${total:.2f}"
+
+
+# list1 = groceries
+
+
+# for item in list1:
+#     quantity = int(input(f"How many units would you like of this item {item[0]}? "))
+#     item.append(quantity)
+#     total_unit_cost = float(calculate_cost(item[1],quantity))
+#     item.append(total_unit_cost)
+# /
+
+# for item in list1:
+#     print(calculate_cost(unit_price, number_purchase))
+
+
+
+
+# # print("====Izzy's Food Emporium====")
+# # for items in list1:
+# #     print(f"{item:<16} ${total_unit_cost:.2f}")
+# #     print("============================")
+# #     # print(f"{total_unit_cost:>25}")
+
+
 
 # total_unit_cost = calculate_cost
 # print(calculate_cost)
@@ -91,11 +169,7 @@ for item in list1:
 # for item in groceries:
 #     print
 
-# print("====Izzy's Food Emporium====")
-# for item in groceries: 
-#     (f"{item[0]:<16} ${calculate_cost(unit_price, number_purchase):.2f}")
-# print("============================")
-# print(f"{total_:>25}")
+
 
 
 # print("====Izzy's Food Emporium====")
